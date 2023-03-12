d0f14c8e828b038e01442b2b22ebc36a9af8ec86
Khalil732003 <100374222+Khalil732003@users.noreply.github.com>
2023-03-12 12:29:34 +0200

    designed the search bar and filled the settings page

--- app/src/main/java/com/example/recipeapp/SettingsFragment.java
+++ app/src/main/java/com/example/recipeapp/SettingsFragment.java
@@ -1,7 +1,9 @@
 package com.example.recipeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
