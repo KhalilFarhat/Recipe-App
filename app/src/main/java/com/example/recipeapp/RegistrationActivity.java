310fee084cd84a7dfbc1248ece0269e59ed17496
ayaelsaoudi01 <ayasphoneemail2001@gmail.com>
2023-03-19 17:17:24 +0200

    register

--- app/src/main/java/com/example/recipeapp/RegistrationActivity.java
+++ app/src/main/java/com/example/recipeapp/RegistrationActivity.java
@@ -2,16 +2,29 @@ package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    EditText input_name, input_email, input_password;
+    Button register_btn;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        register_btn=findViewById(R.id.register_btn);
+        input_name=findViewById(R.id.input_name);
+        input_email=findViewById(R.id.input_email);
+        input_password=findViewById(R.id.input_password);
+
     }
 
     public void back(View view){
---
+++ app/src/main/java/com/example/recipeapp/dbHelper.java
@@ -0,0 +1,59 @@
+package com.example.recipeapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class dbHelper extends SQLiteOpenHelper {
+
+
+    private Context context;
+    private static String DATABASE_NAME = "MyDataBase.db";
+    private static int DATABASE_VERSION = 1;
+
+    private static String TABLE_NAME = "Users";
+    private static String COLUMN_id = "user_id";
+    private static String COLUMN_NAME = "user_name";
+    private static String COLUMN_EMAIL = "user_email";
+    private static String COLUMN_PASSWORD = "user_password" +
+            "";
+    public dbHelper(@Nullable Context context) {
+        super(context,  DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query =
+                "CREATE TABLE " + TABLE_NAME +
+                        " ("+ COLUMN_id + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_NAME + " TEXT, " +
+                        COLUMN_EMAIL + " TEXT, "+
+                        COLUMN_PASSWORD + " TEXT);";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+    void addUser(String name, String email, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_NAME, name);
+        cv.put(COLUMN_EMAIL, email);
+        long result = db.insert(TABLE_NAME, null, cv);
+        if (result == -1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(context, "Added Successfully", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
